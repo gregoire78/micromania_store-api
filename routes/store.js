b5f84ade8c02e188e5b46f26bb0af3698725e9b0
ITELIOS\gjoncour <gregoire.joncour@itelios.com>
2019-07-18 16:13:44 +0200

    format data store

--- routes/store.js
+++ routes/store.js
@@ -5,16 +5,16 @@ async function getStore(ctx) {
     try {
         const storePage = await axios.get(ctx.request.body.url)
         const $ = cheerio.load(storePage.data)
-        const title = $('.page-title h1').text().trim().replace(/\s+\n\s+/g, '\n')
-        const address = $('.address').text().trim().replace(/\s+\n\s+/g, '\n')
-        const phone = $('.phone').text().trim().replace(/\s+\n\s+/g, '\n')
+        const title = $('.page-title h1').text().trim().replace(/\s+\n\s+/g, '')
+        const address = $('.address').text().trim().replace(/\s+\n\s+/g, '<br/>')
+        const phone = $('.phone').text().trim().replace(/\s+\n\s+/g, '')
         const img = $('.slides img').attr('src')
-        const status = $('#shopStatus').text().trim().replace(/\s+\n\s+/g, '\n')
+        const status = $('#shopStatus').text().trim().replace(/\s+\n\s+/g, '')
         const hours = $('#shopHour>li').toArray().map((x) => {
             return $(x).children().toArray().map((x) => {
                 return $(x).text()
             })
-        })
+        }).filter((h) => { return h.length > 0 })
         ctx.ok({
             title,
             address,
